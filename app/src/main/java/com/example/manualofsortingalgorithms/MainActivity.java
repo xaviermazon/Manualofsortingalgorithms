6ee7da6503082048390b9d028cf6b96de6ff129d
Xavier Mazon <xavi.mazon.bcn@gmail.com>
2022-11-21 16:59:38 +0100

    Plantilla inicial para testear los algortimos de ordenacion

--- app/src/main/java/com/example/manualofsortingalgorithms/MainActivity.java
+++ app/src/main/java/com/example/manualofsortingalgorithms/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.manualofsortingalgorithms;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linlay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        int array[] = new int[] {11,12,13,14,15,16,17,18,19,0,1,2,3,4,5,6,7,8,9,10};
+
+        linlay = new LinearLayout(this);
+        linlay.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        linlay.setOrientation(LinearLayout.VERTICAL);
+        linlay.setGravity(Gravity.CENTER_HORIZONTAL);
+        TextView txtAnuncio = new TextView(this);
+        txtAnuncio.setText("Choose a sort's algorithm");
+        linlay.addView(txtAnuncio);
+
+        String bufArray = "<[";
+        Log.e("ERROR ",String.valueOf(array.length));
+        for(int i = 0; i < array.length; i++) {
+            bufArray = bufArray + Integer.toString(array[i]);
+            if(i < array.length-1) bufArray += ", ";
+            else bufArray += "]>";
+        }
+        Log.e("ERROR ",bufArray);
+        TextView txtArrayUnsorted = new TextView(this);
+        txtArrayUnsorted.setText(bufArray);
+        linlay.addView(txtArrayUnsorted);
+
+        Button btnSort = new Button(this);
+        btnSort.setText("Sort array!");
+        btnSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Info","Pendent call function sort");
+            }
+        });
+        linlay.addView(btnSort);
+
+        bufArray = "";
+        bufArray = "<[";
+        Log.e("ERROR ",String.valueOf(array.length));
+        for(int i = 0; i < array.length; i++) {
+            bufArray = bufArray + Integer.toString(array[i]);
+            if(i < array.length-1) bufArray += ", ";
+            else bufArray += "]>";
+        }
+        Log.e("ERROR ",bufArray);
+        TextView txtArraySorted = new TextView(this);
+        txtArraySorted.setText(bufArray);
+        linlay.addView(txtArraySorted);
+
+        setContentView(linlay);
     }
 }

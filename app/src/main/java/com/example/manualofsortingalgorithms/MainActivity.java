dddbb4e34c0ed83b92dc37878dc11482a056a69a
Xavier Mazon <xavi.mazon.bcn@gmail.com>
2022-11-28 19:23:33 +0100

    Add new intent, imageView and commented feature rotate screen

--- app/src/main/java/com/example/manualofsortingalgorithms/MainActivity.java
+++ app/src/main/java/com/example/manualofsortingalgorithms/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.manualofsortingalgorithms;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,68 +26,42 @@ public class MainActivity extends AppCompatActivity {
     int[] array = new int[]{11, 12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     String bufArray = "";
     TextView txtArraySorted;
+    Configuration config;
+    ImageView imageView;
+    Bitmap bitmap;
+    Canvas canvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
+        config = this.getResources().getConfiguration();
+
         linlay = new LinearLayout(this);
         linlay.setLayoutParams(new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
+
+        //if(this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+        //    config.orientation = Configuration.ORIENTATION_LANDSCAPE;
+        //    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //}
+
         linlay.setOrientation(LinearLayout.VERTICAL);
         linlay.setGravity(Gravity.CENTER_HORIZONTAL);
-        TextView txtAnuncio = new TextView(this);
-        txtAnuncio.setText("Choose a sort's algorithm");
-        linlay.addView(txtAnuncio);
-
-        bufArray = "<[";
-        Log.e("ERROR ",String.valueOf(array.length));
-        for(int i = 0; i < array.length; i++) {
-            bufArray = bufArray + Integer.toString(array[i]);
-            if(i < array.length-1) bufArray += ", ";
-            else bufArray += "]>";
-        }
-        Log.e("ERROR ",bufArray);
-        TextView txtArrayUnsorted = new TextView(this);
-        txtArrayUnsorted.setText(bufArray);
-        linlay.addView(txtArrayUnsorted);
-
-        Button btnSort = new Button(this);
-        btnSort.setText("Sort array!");
-        btnSort.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                QuickSort qSort = new QuickSort(array);
-                qSort.sort();
-                array = qSort.getArray();
-                bufArray = "";
-                bufArray = "<[";
-                Log.e("ERROR ",String.valueOf(array.length));
-                for(int i = 0; i < array.length; i++) {
-                    bufArray = bufArray + Integer.toString(array[i]);
-                    if(i < array.length-1) bufArray += ", ";
-                    else bufArray += "]>";
-                }
-                Log.e("ERROR ",bufArray);
-                txtArraySorted.setText(bufArray);
-            }
-        });
-        linlay.addView(btnSort);
-
-        bufArray = "";
-        bufArray = "<[";
-        Log.e("ERROR ",String.valueOf(array.length));
-        for(int i = 0; i < array.length; i++) {
-            bufArray = bufArray + Integer.toString(array[i]);
-            if(i < array.length-1) bufArray += ", ";
-            else bufArray += "]>";
-        }
-        Log.e("ERROR ",bufArray);
-        txtArraySorted = new TextView(this);
-        txtArraySorted.setText(bufArray);
-        linlay.addView(txtArraySorted);
+        imageView = new ImageView(this);
+        linlay.addView(imageView);
+        Point p = new Point();
+        getWindowManager().getDefaultDisplay().getSize(p);
+        int size = (int) (p.y * 0.6);
+        Log.e("Dimensions", "onCreate: X:"+p.x+" p.y:"+p.y);
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(p.x,size));
+        bitmap = Bitmap.createBitmap(p.x, size, Bitmap.Config.ARGB_8888);
+        imageView.setImageBitmap(bitmap);
+        canvas = new Canvas();
+        canvas.setBitmap(bitmap);
+        canvas.drawColor(Color.GRAY);
 
         setContentView(linlay);
     }
